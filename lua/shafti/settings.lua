60b4c652da46bee5347c9d0bcf09b9696cf92048
Shafti <shafti.code@gmail.com>
2025-01-28 07:36:38 +0100

    themery keybind

--- lua/shafti/settings.lua
+++ lua/shafti/settings.lua
@@ -6,6 +6,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
+vim.keymap.set('n', '<leader>th', ":Themery<CR>")
+
 local harpoon = require("harpoon")
 
 -- REQUIRED
