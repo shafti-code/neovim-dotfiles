1dbe8dce13b82058f6bf0881cb8e5f556502c4f7
shafti-code <shafti.code@gmail.com>
2025-02-13 07:16:22 +0100

    neovim is now transparent

--- lua/shafti/settings.lua
+++ lua/shafti/settings.lua
@@ -46,5 +46,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-
 vim.opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none'})
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none'})
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none'})
