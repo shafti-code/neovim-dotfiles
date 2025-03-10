91d77a3d46ade2c22a7bc1b86fe9c75ad17a898d
shafti <shafti.code@gmail.com>
2025-03-10 15:26:25 +0100

    maintenence

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,12 +1,14 @@
 return {
     "zaldih/themery.nvim",
+    priority = 1000,
     lazy = false,
+
     config = function()
-      require("themery").setup({
-          themes = {"rose-pine","ashen","night-owl"},
-          livePreview = true,
-        -- add the config here
-      })
+        require("themery").setup({
+            themes = {"night-owl","rose-pine","ashen"},
+            livePreview = true,
+            -- add the config here
+        })
     end
-  }
+}
 
