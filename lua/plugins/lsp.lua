b0c26414e3f6668e96b63900050d243612a37413
shafti <shafti.code@gmail.com>
2025-04-13 22:53:14 +0200

    rust analyzer, we gonna try rust guys

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,13 +17,15 @@ return {
         },
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
-            require("lspconfig").lua_ls.setup { capabilities = capabilities }
+            require("lspconfig").phpactor.setup{ capabilities = capabilities }
+            require("lspconfig").clangd.setup { capabilities = capabilities }
             require("lspconfig").gopls.setup { capabilities = capabilities }
             require("lspconfig").html.setup { capabilities = capabilities }
-            require("lspconfig").clangd.setup { capabilities = capabilities }
+            require("lspconfig").lua_ls.setup { capabilities = capabilities }
+            require("lspconfig").pyright.setup { capabilities = capabilities }
             require("lspconfig").ts_ls.setup { capabilities = capabilities }
+            require("lspconfig").rust_analyzer.setup { capabilities = capabilities }
             require("lspconfig").zls.setup { capabilities = capabilities }
-            require'lspconfig'.phpactor.setup{ capabilities = capabilities }
         end,
     }
 
