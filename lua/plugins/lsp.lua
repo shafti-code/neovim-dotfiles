b7a7f1c8c80a190470407cd62d6237f1fb346dc7
shafti <shafti.code@gmail.com>
2025-04-19 12:01:18 +0200

    super minor changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ return {
             require("lspconfig").gopls.setup { capabilities = capabilities }
             require("lspconfig").html.setup { capabilities = capabilities }
             require("lspconfig").lua_ls.setup { capabilities = capabilities }
-            require("lspconfig").pyright.setup { capabilities = capabilities }
+            -- require("lspconfig").pyright.setup { capabilities = capabilities }
             require("lspconfig").ts_ls.setup { capabilities = capabilities }
             require("lspconfig").rust_analyzer.setup { capabilities = capabilities }
             require("lspconfig").zls.setup { capabilities = capabilities }
