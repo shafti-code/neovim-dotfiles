fb6bc10cb57418ea1815adf0b75f47909d1044b2
Shafti <shafti@Shaftis-MacBook-Pro.local>
2025-07-14 23:52:05 +0200

    fix: plugins now live in settings folder as per request

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,8 +24,8 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
-    { import = "plugins" },
+    -- import your plugins from settings
+    { import = "settings" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
