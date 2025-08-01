8e008980015fdc4e5627c9c5a9c12ed46c549e6d
shafti-code <shafti.code@gmail.com>
2025-08-01 23:26:06 +0200

    initial state of the minmal branch

--- init.lua
+++ init.lua
@@ -1,4 +1,21 @@
-require("config.lazy")
-require("shafti")
-
-
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.isfname:append("@-@")
+vim.opt.smartindent = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.mouse = ""
+vim.opt.wrap = false
+vim.o.termguicolors = true
+vim.opt.list=true
+vim.opt.listchars="space:·"
