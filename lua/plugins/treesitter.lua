430afa47be563a561b0234293f18c698723c495c
shafti-code <shafti.code@gmail.com>
2026-03-16 13:59:15 +0100

    no longer schitzophrenic install all tresitter policies

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = "all",
+        ensure_installed = {"c","go","typescript","ruby","bash","php"},
         ignore_install = {"ipkg"},
         highlight = { enable = true },
         incremental_selection = { enable = true },
